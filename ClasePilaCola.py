58ded05b6f845fd9943efb46ae725a4b6364bc52
Yoh-Ortiz <30814410+Yoh-Ortiz@users.noreply.github.com>
2018-08-20 19:11:42 -0500

    Update ClasePilaCola.py

--- ClasePilaCola.py
+++ ClasePilaCola.py
@@ -49,26 +49,46 @@ class Pila:
     def es_vacia(self):
         """ Devuelve True si la lista está vacía, False si no. """
         return self.items == []
-    
-    
-class Libro():
-    titulo = ""
-    autor = ""
-    categoria = ""
 
+
+class Libro:
+
+    def __init__(self,titulo,autor,genero):
+        self.titulo=titulo
+        self.autor=autor
+        self.genero=genero
+        
 class Moto ():
     placa = ""
     FichaPropiedad = ""
     Horario = ""
     CodigoCarnet = ""
 
-    
 cola = Cola()
-
 archivo = open("libros.csv", "r")
 lista = [(x.split(";")[0],x.split(";")[1],x.split(";")[2]) for x in archivo.readlines()]
 for x in lista:
-    cola.encolar(x)
-print(lista)
-
-    
+        cola.encolar(x)
+print(archivo.read(5))
+while True:
+    opcion = int(input(" 1. Mostrar todos los libros \n 2.Extraer libro por título \n 3. Extraer libros por autor \n 4. Extraer libros por categoría \n 5. consultar libros extraidos \n 6. apilar libros extraidos \n 7. Salir \n"))
+    print("Accion:",opcion)
+    if (opcion==4):
+        break
+    elif opcion==1:
+        while cola.es_vacia() != True:
+                print(cola.desencolar())
+    elif opcion==2:
+        colaTitulos = Cola()
+        print(cola.items)
+    elif opcion==3:
+        colaAutores = Cola()
+        print(cola.items)
+    elif opcion==4:
+        colaCategoria = Cola()
+        print(cola.items)
+    elif opcion==6:
+        archivo = open("libros.csv", "r")
+        lista = [(x.split(";")[0],x.split(";")[1],x.split(";")[2]) for x in archivo.readlines()]
+        for x in lista:
+            cola.encolar(x)
