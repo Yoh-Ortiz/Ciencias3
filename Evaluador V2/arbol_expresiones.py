f6730c7465062e6fe6d13217a7577a6789b98a18
oscarTopinOne <30842893+oscarTopinOne@users.noreply.github.com>
2018-08-27 23:55:45 -0500

    Evaluador de Expresiones Versión 2

--- Evaluador V2/arbol_expresiones.py
+++ Evaluador V2/arbol_expresiones.py
@@ -1,18 +1,24 @@
 from pila import *
 from arbol import *
 
+
+def tupla(valor, letra):
+    tuplas_enc.append((letra, valor))
+
 def convertir(lista, pila):
     if lista != []:
-        if lista[0] in "+-*/=":
+        if lista[0] in "+-/*=":
             nodo_der = pila.desapilar()
             nodo_izq = pila.desapilar()
             pila.apilar(Nodo(lista[0],nodo_izq,nodo_der))
         else:
             pila.apilar(Nodo(lista[0]))
         return convertir(lista[1:],pila)
-            
+
+         
 
 def evaluar(arbol):
+    conjunto= "abc"
     if arbol.valor == "+":
         return evaluar(arbol.izq) + evaluar(arbol.der)
     if arbol.valor == "-":
@@ -21,20 +27,17 @@ def evaluar(arbol):
         return evaluar(arbol.izq) / evaluar(arbol.der)
     if arbol.valor == "*":
         return evaluar(arbol.izq) * evaluar(arbol.der)
+    if arbol.valor in conjunto:
+         for i in range(len(tuplas_enc)):
+            if tuplas_enc[i][0] == arbol.valor:
+                return tuplas_enc[i][1]
     if arbol.valor == "=":
-        return evaluar(arbol.izq)
+        return tupla(evaluar(arbol.izq), arbol.der.valor)
     return int(arbol.valor)
 
-class diccionario():
-    def __init__(self, lista=None):
-        self.lista=[]
-    def agregar(string,int):
-        self.lista.append((string,int))
-
-dic = diccionario()
 pilaEnc = Pila()
 resp = []
-
+tuplas_enc =[]
 archivo = open("expresiones.in", "r")
 lista = []
 lista_esc =[]
@@ -45,11 +48,11 @@ for x in lista:
         exp = x.split(" ")
         convertir(exp, pilaEnc)
         respuesta =(evaluar(pilaEnc.desapilar()))
-        print(respuesta)
-        lista_esc.append(str(respuesta))
-
+        
+        
 arc_salida=open("expresiones.out","w")
-for i in range(0,len(lista_esc)):
-    arc_salida.write(lista_esc[i]+"\n")
+for i in range(0,len(tuplas_enc)):
+    arc_salida.write(tuplas_enc[i][0]+"="+str(tuplas_enc[i][1])+"\n")
     
 arc_salida.close()
+print (tuplas_enc)
