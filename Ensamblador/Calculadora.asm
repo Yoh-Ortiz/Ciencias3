cbec30ad409109daba6af996e8c60f8bd2976b07
oscarTopinOne <30842893+oscarTopinOne@users.noreply.github.com>
2018-09-10 20:56:21 -0500

    Se agregan comentarios

--- Ensamblador/Calculadora.asm
+++ Ensamblador/Calculadora.asm
@@ -1,28 +1,28 @@
-org 100h ; inicio de programa
+org 100h ; inicio de programa arquitectura para el formato 8086
 include 'emu8086.inc' ;Incluye funciones de libreria emu8086 
-; DEFINE_SCAN_NUM
-; DEFINE_PRINT_STRING
- DEFINE_PRINT_NUM
- DEFINE_PRINT_NUM_UNS
+  DEFINE_PRINT_NUM
+  DEFINE_PRINT_NUM_UNS  ;Funciones para imprimir 
+
 mov si, 0
 mov al, 0
+
 ;Convertir primera cadena a numero
  cadAnum:
-cmp cad1[si], "$"
- jz seguir
+cmp cad1[si], "$" ;si cadena es vacia sigue
+ jz seguir        ;accion de continuar
 
- mov bl, 10 
- mul bl
-sub cad1[si], '0' 
+ mov bl, 10       ;bl=10
+ mul bl           ;multiplicar
+sub cad1[si], '0' ;restar sin llevar 
  add al, cad1[si]
 
- inc si
-loop cadAnum
+ inc si      ;incrementa si contador
+loop cadAnum ;se termina el ciclo y vuelve
 
 seguir:
-mov aux1, al
+mov aux1, al ;aux1 = al
 ;Convertir segunda cadena a numero
-mov si, 0
+mov si, 0   ; se vuelven 0 las variables
 mov al, 0
 cadAnum2:
 cmp cad3[si], "$"
@@ -34,13 +34,13 @@ sub cad3[si], '0'
  add al, cad3[si]
 
  inc si
-loop cadAnum2
+loop cadAnum2    ; es el la misma funcion de arriba pero con la cadena 2
 
 seguir2:
-mov bl, al 
-mov al, aux1
+mov bl, al    ;bl=al
+mov al, aux1  ;al= aux1
 ;realizar operaciones normalmente teniendo ya los dos numeros decimales
-cmp cad2, "-"
+cmp cad2, "-"   ;comparacion de posibles signos de operacion
 jz resta 
 cmp cad2, "+"
 jz suma 
@@ -49,29 +49,29 @@ jz multi
 cmp cad2, "/"
 jz divi
 resta: 
-sub al, bl
+sub al, bl  ;operacion resta
 jmp fin
 suma: 
-add al, bl
+add al, bl  ;operacion suma
 jmp fin
 multi: 
-mul bl
+mul bl      ;operacion multiplicacion
 jmp fin
 divi: 
-div bl
+div bl      ;operacion division
 jmp fin
 
 fin:
 mov bx, ax
 mov ah,09 
-lea dx,msg
+lea dx,msg    ; desplazamiento en memoria del mensaje
 int 21h 
 mov ax, bx
-call PRINT_NUM
+call PRINT_NUM ;llama la funcion imprimir
 ret
-cad1 db "7$"
-cad2 db "*"
-cad3 db "5$"
-aux1 db ?
-aux2 dw ?
-msg dw "El resultado es: $"
+cad1 db "10$"  ;cadenas asociadas para el calculo
+cad2 db "-"
+cad3 db "2$"
+aux1 db ?     ;variables no inicializadas
+aux2 dw ? 
+msg dw "El resultado es: $" ;imprime el resultado
